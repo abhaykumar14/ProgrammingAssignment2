ff54af643489951d84de176bd82b323a6d17fd58
Abhay Kumar <abhay14111983@gmail.com>
2015-10-24 13:41:35 +0100

    Final R File

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
 ## This programme will create a inverse matrix for square matrix and cache/store in different environment to 
 ## recover it later, rather recalculating it (if the matrix does not change)
-##
+
+
+      ## makeCacheMatrix takes the matrix input and solves the inverse of the marix.
+      ## The solution and input matrix is cached in a different environment, which can be recalled.
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -15,6 +18,8 @@ makeCacheMatrix <- function(x = matrix()) {
                   list(setmatrix = setmatrix, getmatrix = getmatrix,setinverse = setinverse,getinverse=getinverse)
                   
 }
+  ## cachesolve initially, checks the matrix and its inverse solution in other environment and returns the solution.
+  ## If the inverse solution does not exist then it calculates matrix inverse and sets in the cache.
 
 cacheSolve <- function(x, ...) { ## Return a matrix that is the inverse of 'x'
                 m<-x$getinverse()
