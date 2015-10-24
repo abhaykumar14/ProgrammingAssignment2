93ac1a89de7e7da74558b0d3e0edb37a6cffc2cc
Abhay Kumar <abhay14111983@gmail.com>
2015-10-24 12:48:09 +0100

    edited file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
+## This programme will create a inverse matrix for square matrix and cache/store in different environment to 
+## recover it later, rather recalculating it (if the matrix does not change)
 ## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+                  m<-NULL
+                  setmatrix<-function(y){
+                  x<<-y
+                  m<<-NULL
+              }
+                  getmatrix<-function() x
+                  setinverse<-function(solve) m<<- solve
+                  getinverse<-function() m
+                  list(setmatrix = setmatrix, getmatrix = getmatrix,setinverse = setinverse,getinverse=getinverse)
+                  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ## Return a matrix that is the inverse of 'x'
+                m<-x$getinverse()
+                if(!is.null(m)){
+                  message("getting cached data")
+                  return()
+                }
+                data<-x$getmatrix()                
+                m<- solve(data)
+                x$setinverse(m)
+                m
 }
